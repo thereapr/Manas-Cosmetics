7b0f34ae19faf68afd3385cf071377aa5e286a1f
thereapr <bornaamital@gmail.com>
2026-04-12 16:40:02 +0200

    GUI no longer blurry, slot list vertical now.

--- neoforge/src/main/java/io/github/manasmods/manas_cosmetics/neoforge/ManasCosmeticsNeoForge.java
+++ neoforge/src/main/java/io/github/manasmods/manas_cosmetics/neoforge/ManasCosmeticsNeoForge.java
@@ -1,16 +1,12 @@
 package io.github.manasmods.manas_cosmetics.neoforge;
 
 import io.github.manasmods.manas_cosmetics.ManasCosmetics;
-import io.github.manasmods.manas_cosmetics.client.renderer.ClientCosmeticModelCache;
-import io.github.manasmods.manas_cosmetics.client.renderer.CosmeticLayer;
 import io.github.manasmods.manas_cosmetics.entity.EntityRegistry;
 import io.github.manasmods.manas_cosmetics.entity.PetCosmeticEntity;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 
 @Mod(ManasCosmetics.MOD_ID)
@@ -24,19 +20,10 @@ public final class ManasCosmeticsNeoForge {
 
         if (FMLEnvironment.dist == Dist.CLIENT) {
             io.github.manasmods.manas_cosmetics.client.ManasCosmticsClient.init();
-            modBus.addListener(ManasCosmeticsNeoForge::registerRenderLayers);
         }
     }
 
     private static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(EntityRegistry.PET_COSMETIC.get(), PetCosmeticEntity.createAttributes().build());
-    }
-
-    /** Attaches CosmeticLayer to both the default and slim player renderers. */
-    @OnlyIn(Dist.CLIENT)
-    private static void registerRenderLayers(EntityRenderersEvent.AddLayers event) {
-        event.getSkinMap().forEach((skin, playerRenderer) ->
-            playerRenderer.addLayer(new CosmeticLayer<>(playerRenderer, ClientCosmeticModelCache.get()))
-        );
     }
 }
